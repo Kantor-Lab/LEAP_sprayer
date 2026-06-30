8df07a54ba11fa3fe60fcf7e54d44f965f82ccc1
Evan Foster <evanfost@andrew.cmu.edu>
2026-06-30 15:43:38 -0400

    create camera package
    can store a bunch of camera implementations

---
+++ src/camera/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'camera'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Evan',
+    maintainer_email='evanfost@andrew.cmu.edu',
+    description='A collection of several camera nodes that can be used for weed detection',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
