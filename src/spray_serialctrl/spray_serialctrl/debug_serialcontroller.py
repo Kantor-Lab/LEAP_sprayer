5771d2384e9d133484eb64c0f880731163c2712b
Evan Foster <evanfost@andrew.cmu.edu>
2026-07-20 15:35:29 -0400

    visualize serial controller pulses
    much nicer for confirming stuff is firing where it should

--- src/spray_serialctrl/spray_serialctrl/debug_serialcontroller.py
+++ src/spray_serialctrl/spray_serialctrl/debug_serialcontroller.py
@@ -1,6 +1,8 @@
+from geometry_msgs.msg import Vector3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import ColorRGBA, Header, String
+from visualization_msgs.msg import Marker, MarkerArray
 
 from .serialcontroller import validate_cmd
 
@@ -25,8 +27,30 @@ class SpraySerialController(Node):
             self.listener_callback,
             10
         )
+        self.markers_pub = self.create_publisher(
+            MarkerArray,
+            'serial_nozzle_markers',
+            10
+        )
 
         self.nozzles = ['◯'] * (find_max_boom_index() + 1)
+        self.markers = [Marker(
+            header=Header(
+                frame_id=f'spot_nozzle{index}'
+            ),
+            id=index,
+            type = Marker.SPHERE,
+            action=Marker.DELETE,
+            color=ColorRGBA(
+                r=1.0,
+                a=1.0
+            ),
+            scale=Vector3(
+                x=0.1,
+                y=0.1,
+                z=0.1
+            ),
+        ) for index in range(find_max_boom_index() + 1)]
 
     def listener_callback(self, msg: String):
         is_valid = False
@@ -43,12 +67,21 @@ class SpraySerialController(Node):
                 else:
                     if msg.data[4] == '0':
                         self.nozzles[int(msg.data[int(3)])] = '◯'
+                        self.markers[int(msg.data[int(3)])].action = Marker.DELETE
                     elif msg.data[4] == '1':
                         self.nozzles[int(msg.data[int(3)])] = '⬤'
+                        self.markers[int(msg.data[int(3)])].action = Marker.ADD
+                        
                     else:
                         raise ValueError(f"Invalid nozzle state: {msg.data[4]}")
-                
+
                 self.get_logger().info(f"Sprayer state (cmd: {msg.data}): {' '.join(self.nozzles)}")
+                now_msg = self.get_clock().now().to_msg()
+                for i in range(len(self.markers)):
+                    self.markers[i].header.stamp = now_msg
+                self.markers_pub.publish(MarkerArray(
+                    markers=self.markers
+                ))
             except IndexError | ValueError:
                 self.get_logger().error(f"Failed to update nozzle state: {msg.data}")
         else:
